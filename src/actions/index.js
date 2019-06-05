619abf67dcd23f4335bee4d8b9ff619b72da301f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-05 14:54:02 +0200

    removed unnecessary exports from action file

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,13 +4,13 @@ export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_ERROR = "FETCH_ERROR";
 
 // action creators - fetching data
-export const fetch_request = () => {
+const fetch_request = () => {
   return { type: FETCH_REQUEST };
 }
-export const fetch_success = (data) => {
+const fetch_success = (data) => {
   return { type: FETCH_SUCCESS, weather: data };
 }
-export const fetch_error = (error) => {
+const fetch_error = (error) => {
   return { type: FETCH_ERROR, error: error };
 }
 
